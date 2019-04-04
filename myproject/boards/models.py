0910ce7e452c7eff8b661a83a4b28f2d38e084a0
Chengjigeer Bao <46412517+CJGEB@users.noreply.github.com>
2019-04-04 18:47:09 -0400

    Update models.py

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -7,25 +7,6 @@ from django.contrib.auth.models import User
 
 from django.core.validators import MinLengthValidator
 
-'''
-class Board(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    description = models.CharField(max_length=100)
-
-class Topic(models.Model):
-    subject = models.CharField(max_length=255)
-    last_updated = models.DateTimeField(auto_now_add=True)
-    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='topics')
-    starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
-
-class Post(models.Model):
-    message = models.TextField(max_length=4000)
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='posts')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='+')
-'''
 
 class Donor(models.Model):
     # this should be a one-to-many field (i think), have no idea if it's possible...
