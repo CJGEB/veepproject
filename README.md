572e71ec52f3b91da9f5330be0cec995a8c60fa8
Daniel Xu <danielxu0818@gmail.com>
2019-06-29 23:35:51 -0400

    backup code

--- README.md
+++
@@ -1,3 +0,0 @@
-# veepproject
-
-This file contains codes for the VEEP project, also serves as a practice for "A Complete Begineer's Guide to Django" from simpleisbetterthancomplex.com
--- myproject/tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [path('', views.main_tasks_page, name='main_page'),
-               path('<str:option>', views.task_option, name='task_option'),
-               path(r'<str:option>/<str:task>', views.task_forms, name='forms')]
-
