163df09dee957907d18f1298695661c6d207d9a9
ShaziahGafur <shaziah_g7@hotmail.com>
2019-07-20 11:48:15 -0400

    old migrations deleted

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -71,7 +71,7 @@ class Item(models.Model):
     POWER_TEST = [('Yes', 'Pass.'),
     ('No', 'Fail.')]
     item_type = models.CharField(max_length = 200) # , choices = ITEM_TYPE)
-    power_test = models.CharField(max_length=100, choices=POWER_TEST,default =None)
+    power_test = models.CharField(max_length=100, choices=POWER_TEST,default='No')
     recieved_date = models.DateTimeField(auto_now_add = True)
     starter = models.ForeignKey(User, on_delete=models.CASCADE, default=-1, related_name='items')
 
