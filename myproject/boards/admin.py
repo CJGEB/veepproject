ee99adf82b8506e036036174ee6e842fadeac275
ShaziahGafur <shaziah_g7@hotmail.com>
2019-08-03 12:31:04 -0400

    admin page with item and types

--- myproject/boards/admin.py
+++ myproject/boards/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 # Register your models here.
 
 # from .models import Item
-from .models import Donor
-admin.site.register(Donor)
+from .models import Donor, Item, Type
+
+
+# @admin.site.register(Donor)
+@admin.register(Donor)
+class DonorAdmin(admin.ModelAdmin):
+    list_display = ['donorname', 'invoicenum', 'number_of_donations', 'last_updated']
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['manufacturer', 'item_type', 'item_model']
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['serialnum', 'serialnum', 'operating_system']
